9bd2524c51453800cbbf5b8c7a9dca23521c3a0d
Колосс Матвій <kolossmatveyka@gmail.com>
2023-01-23 01:32:24 +0200

    edit

--- SportSections/wwwroot/js/site.js
+++ SportSections/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function createPDF(title) {
+    var sTable = $('#print').html()
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 17px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + ".createPdfBlock {display: none;}";
+    style = style + "</style>";
 
-// Write your JavaScript code.
+    var win = window.open('', '', 'height=700,width=700');
+    win.document.write('<html><head>');
+    win.document.write(`<title>${title}</title>`);
+    win.document.write(style);
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.write(`<h1 style='text-align: center;'>${title}</h1>`);
+    win.document.write(sTable);
+    win.document.write('</body></html>');
+    win.document.close();
+    win.print();
+}

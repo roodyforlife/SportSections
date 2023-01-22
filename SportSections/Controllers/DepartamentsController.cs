a0196f542e12d9f0896677b73843fadf9aaa3320
Колосс Матвій <kolossmatveyka@gmail.com>
2023-01-22 17:36:31 +0200

    edit

---
+++ SportSections/Controllers/DepartamentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportSections.DataBase;
+using SportSections.Models;
+
+namespace SportSections.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public DepartamentsController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Departaments
+        public async Task<IActionResult> Index()
+        {
+            var dataBaseContext = _context.Departaments.Include(d => d.Faculty);
+            return View(await dataBaseContext.ToListAsync());
+        }
+
+        // GET: Departaments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departament = await _context.Departaments
+                .Include(d => d.Faculty)
+                .FirstOrDefaultAsync(m => m.DepartamentId == id);
+            if (departament == null)
+            {
+                return NotFound();
+            }
+
+            return View(departament);
+        }
+
+        // GET: Departaments/Create
+        public IActionResult Create()
+        {
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FullName");
+            return View();
+        }
+
+        // POST: Departaments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartamentId,ShortName,FullName,FacultyId")] Departament departament)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departament);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FullName", departament.FacultyId);
+            return View(departament);
+        }
+
+        // GET: Departaments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departament = await _context.Departaments.FindAsync(id);
+            if (departament == null)
+            {
+                return NotFound();
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FullName", departament.FacultyId);
+            return View(departament);
+        }
+
+        // POST: Departaments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DepartamentId,ShortName,FullName,FacultyId")] Departament departament)
+        {
+            if (id != departament.DepartamentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departament);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentExists(departament.DepartamentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FullName", departament.FacultyId);
+            return View(departament);
+        }
+
+        // GET: Departaments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departament = await _context.Departaments
+                .Include(d => d.Faculty)
+                .FirstOrDefaultAsync(m => m.DepartamentId == id);
+            if (departament == null)
+            {
+                return NotFound();
+            }
+
+            return View(departament);
+        }
+
+        // POST: Departaments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var departament = await _context.Departaments.FindAsync(id);
+            _context.Departaments.Remove(departament);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentExists(int id)
+        {
+            return _context.Departaments.Any(e => e.DepartamentId == id);
+        }
+    }
+}
---
+++ SportSections/Controllers/FacultiesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SportSections.DataBase;
+using SportSections.Models;
+
+namespace SportSections.Controllers
+{
+    public class FacultiesController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public FacultiesController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Faculties
+        public async Task<IActionResult> Index()
+        {
+            var dataBaseContext = _context.Faculties.Include(f => f.University);
+            return View(await dataBaseContext.ToListAsync());
+        }
+
+        // GET: Faculties/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .Include(f => f.University)
+                .FirstOrDefaultAsync(m => m.FacultyId == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Faculties/Create
+        public IActionResult Create()
+        {
+            ViewData["UniversityId"] = new SelectList(_context.Universities, "UniversityId", "Address");
+            return View();
+        }
+
+        // POST: Faculties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FacultyId,ShortName,FullName,UniversityId")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(faculty);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UniversityId"] = new SelectList(_context.Universities, "UniversityId", "Address", faculty.UniversityId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+            ViewData["UniversityId"] = new SelectList(_context.Universities, "UniversityId", "Address", faculty.UniversityId);
+            return View(faculty);
+        }
+
+        // POST: Faculties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FacultyId,ShortName,FullName,UniversityId")] Faculty faculty)
+        {
+            if (id != faculty.FacultyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(faculty);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacultyExists(faculty.FacultyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UniversityId"] = new SelectList(_context.Universities, "UniversityId", "Address", faculty.UniversityId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .Include(f => f.University)
+                .FirstOrDefaultAsync(m => m.FacultyId == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // POST: Faculties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var faculty = await _context.Faculties.FindAsync(id);
+            _context.Faculties.Remove(faculty);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacultyExists(int id)
+        {
+            return _context.Faculties.Any(e => e.FacultyId == id);
+        }
+    }
+}
